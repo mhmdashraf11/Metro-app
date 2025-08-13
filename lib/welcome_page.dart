1fa90e1cb6a58900c5df4b9fbcb92a2d95279e22
MohamedAshraf <mohamed4ashraf2019@gmail.com>
2025-08-13 12:37:36 +0300

    intiat

---
+++ lib/welcome_page.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        bottom: false,
+        child: DecoratedBox(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/metro.png'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 128),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: Center(
+                      child: SizedBox(
+                        width: 320,
+
+                        child: Image.asset(
+                          'assets/images/cairo-metro-seeklogo.png',
+                        ),
+                      ),
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.center,
+                    child: Text(
+                      'Welcome to Metro guide',
+                      style: TextStyle(fontSize: 28, color: Colors.white),
+                    ),
+                  ),
+                  Center(
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                        top: 32,
+                        right: 8,
+                        left: 8,
+                      ),
+                      child: SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton.icon(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Color(0xFFc41014),
+                          ),
+                          onPressed: () {},
+                          label: Text(
+                            'Get Started',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          icon: Icon(Icons.arrow_forward, color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
